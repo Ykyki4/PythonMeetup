6097c122e4d44ef6bbc80e64de41f2fdc585c7e5
Ykyki4 <jimmypantalones@gmail.com>
2023-06-20 23:15:38 +0300

    add get user function, fix create user

--- db/utils.py
+++ db/utils.py
@@ -1,19 +1,30 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, select
 from sqlalchemy.orm import Session
-from main import User
+from sqlalchemy.exc import NoResultFound
+from models import User
 
 
 engine = create_engine('sqlite:///db.sqlite3', echo=True)
+session = Session(engine)
 
-def create_user(first_name, last_name, telegram_id)
-	with Session(engine) as session:
 
-		user = User(
-			first_name=first_name,
-	        last_name=last_name
-	        telegram_id=telegram_id,
-	    )
+def get_user(telegram_id):
+	try:
+		stmt = select(User).where(User.telegram_id==telegram_id)
+		user = session.scalars(stmt).one()
+	except NoResultFound:
+		return None
+	return user
 
-		session.add_all([user])
 
-		session.commit()
+def create_user(first_name, last_name, telegram_id):
+	user = User(
+		first_name=first_name,
+        last_name=last_name,
+        telegram_id=telegram_id,
+    )
+
+	session.add_all([user])
+	session.commit()
+
+	return user
