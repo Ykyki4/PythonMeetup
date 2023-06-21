e9a55254de958342bbbfb26630aaf737cf946541
Ykyki4 <jimmypantalones@gmail.com>
2023-06-21 17:54:52 +0300

    change user model

--- backend/models.py
+++ backend/models.py
@@ -5,7 +5,8 @@ class User(models.Model):
     telegram_id = models.CharField('Телеграм идентификатор', max_length=10, unique=True)
     first_name = models.CharField('Имя', max_length=16)
     last_name = models.CharField('Фамилия', max_length=16)
-    about = models.CharField('О себе', max_length=250)
+    is_speaker = models.BooleanField('Спикер', default=False)
+    about = models.CharField('О себе', max_length=250, null=True)
 
     class Meta:
         verbose_name = 'Пользователь'
@@ -15,23 +16,11 @@ class User(models.Model):
         return f"{self.first_name} {self.last_name}"
 
 
-class Guest(User):
-    class Meta:
-        verbose_name = 'Гость'
-        verbose_name_plural = 'Гости'
-
-
-class Speaker(User):
-    class Meta:
-        verbose_name = 'Спикер'
-        verbose_name_plural = 'Спикер'
-
-
 class Event(models.Model):
     title = models.CharField('Заголовок', max_length=100)
     description = models.CharField('Описание', max_length=500)
     time = models.TimeField('Время проведения')
-    speaker = models.ForeignKey(Speaker, on_delete=models.CASCADE, verbose_name='Спикер', related_name='events')
+    speaker = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Спикер', related_name='events')
 
     class Meta:
         verbose_name = 'Событие'
@@ -42,7 +31,7 @@ class Event(models.Model):
 
 
 class Question(models.Model):
-    guest = models.ForeignKey(Guest, on_delete=models.CASCADE, verbose_name='Гость', related_name='questions')
+    guest = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Гость', related_name='questions')
     event = models.ForeignKey(Event, on_delete=models.CASCADE , verbose_name='События', related_name='questions')
     content = models.CharField('Содержание', max_length=250)
 
