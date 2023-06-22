09e265519d91b2042fe75632cf1b9e0a0dfa6c84
Ykyki4 <jimmypantalones@gmail.com>
2023-06-22 14:23:30 +0300

    Add verbose name and related name to visitcard model

--- backend/models.py
+++ backend/models.py
@@ -11,7 +11,7 @@ class User(models.Model):
         verbose_name_plural = 'Пользователи'
 
     def __str__(self):
-        return f"{self.name}"
+        return f'{self.name}'
 
 
 class Event(models.Model):
@@ -25,12 +25,12 @@ class Event(models.Model):
         verbose_name_plural = 'События'
 
     def __str__(self):
-        return f"{self.title}"
+        return f'{self.title}'
 
 
 class Question(models.Model):
     guest = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Гость', related_name='questions')
-    event = models.ForeignKey(Event, on_delete=models.CASCADE , verbose_name='События', related_name='questions')
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, verbose_name='События', related_name='questions')
     content = models.CharField('Содержание', max_length=250)
 
     class Meta:
@@ -38,15 +38,19 @@ class Question(models.Model):
         verbose_name_plural = 'Вопросы'
 
     def __str__(self):
-        return f"Вопрос к {self.event} от {self.guest}"
+        return f'Вопрос к {self.event} от {self.guest}'
 
 
 class VisitCard(models.Model):
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    job_title = models.CharField(max_length=100)
-    phone = models.CharField(max_length=20)
+    owner = models.ForeignKey(User, verbose_name='Владелец', related_name='visit_card', on_delete=models.CASCADE)
+    first_name = models.CharField('Имя', max_length=16)
+    last_name = models.CharField('Фамилия', max_length=16)
+    job_title = models.CharField('Должность', max_length=100)
+    phone = models.CharField('Номер телефона', max_length=20)
+
+    class Meta:
+        verbose_name = 'Визитная карточка'
+        verbose_name_plural = 'Визитные карточки'
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name}'s Visit Card"
+        return f'Визитная карточка {self.first_name} {self.last_name}'
