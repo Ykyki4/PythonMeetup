406cff1805dca63817004a85a3642450b6a7ffa1
Atmoslayer <ddsomdim@gmail.com>
2023-06-26 16:48:15 +0300

    Add main menu message id saving

--- backend/management/commands/telegram_bot/main_menu.py
+++ backend/management/commands/telegram_bot/main_menu.py
@@ -11,14 +11,15 @@ def send_main_menu(update, context):
     query = update.callback_query
     if query:
         context.bot.delete_message(update.effective_chat.id, query.message.message_id)
-        context.bot.send_message(
+        main_menu_message = context.bot.send_message(
             update.effective_chat.id,
             text='Выберите один из следующих пунктов: ',
             reply_markup=get_keyboard(list(main_menu_buttons.values())),
         )
     else:
-        update.message.reply_text(
+        main_menu_message = update.message.reply_text(
             text='Выберите один из следующих пунктов: ',
             reply_markup=get_keyboard(list(main_menu_buttons.values())),
         )
+    context.user_data['main_menu_message_id'] = main_menu_message.message_id
     return MainMenuState.HANDLE_MAIN_MENU
